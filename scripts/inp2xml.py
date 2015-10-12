c36f6bf7ccd0a1a0d8d066e06215d85f88bd0035
Florian Bruckner <e0425375@gmail.com>
2015-10-12 17:02:58 +0200

    added first version of INP file reader as external script

---
+++ scripts/inp2xml.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python
+
+# Copyright (C) 2011-2015 Florian Bruckner
+#
+# This file is part of magnum.msh. 
+#
+# magnum.msh is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# magnum.msh is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with magnum.msh. If not, see <http://www.gnu.org/licenses/>.
+# 
+# Last modified by Claas Abert, 2015-06-11
+
+import argparse
+from dolfin import *
+import numpy as np
+from StringIO import StringIO
+
+class INPConverter(object):
+  def __init__(self): 
+    pass
+
+  def read_inp(self, filename):
+    # TODO: handle element data
+    # TODO: fix problem with node_data datatypes
+    with open(filename, "r") as f:
+      lines = f.readlines()
+      NNodes, NElements, NodeData, ElementData, TensorData = [int(x) for x in lines[0].split()]
+      self.nodes = np.genfromtxt(StringIO(''.join(lines[1:NNodes+1])), dtype=(int, float, float, float))
+      self.elements = np.genfromtxt(StringIO(''.join(lines[NNodes+1:NNodes+NElements+1])), dtype=(int, int, np.dtype((str, 10)), int, int, int, int))
+      
+      X = [int(x) for x in lines[NNodes+NElements+1].split()]
+      NNodeDataComponents = X[0]
+      NodeDataComponenetSizes = X[1:]
+      NodeDataSize = sum(NodeDataComponenetSizes)
+      NodeDataTags = [lines[i].split(',')[0] for i in range(NNodes+NElements+2,NNodes+NElements+NNodeDataComponents+2)]
+      
+      self.node_data = np.genfromtxt(StringIO(''.join(lines[NNodes+NElements+NNodeDataComponents+2:2*NNodes+NElements+NNodeDataComponents+2])), dtype=(float, float))
+    self._need_update = True
+
+  def mesh(self):
+    if self._need_update == True:
+      self._need_update = False
+      editor = MeshEditor()
+      self._mesh = Mesh()
+      editor.open(self._mesh, 3, 3)
+      editor.init_vertices(len(self.nodes))   
+      editor.init_cells(len(self.elements))   
+      map(lambda x: editor.add_vertex(x[0]-1, np.array([x[1], x[2], x[3]])), self.nodes)
+      map(lambda x: editor.add_cell(x[0]-1, np.array([x[3]-1, x[4]-1, x[5]-1, x[6]-1], dtype=np.uintp)), self.elements)
+      editor.close()
+    return self._mesh
+
+  def domains(self):
+    mesh = self.mesh()
+    mat = MeshFunction('size_t', mesh, 3)
+    mat.array()[:] = map(lambda x: x[1], self.elements)
+    return mat
+
+  def data(self, name='data'):
+    mesh = self.mesh()
+    V = FunctionSpace(mesh, "CG", 1)
+    components = []
+    for i in range(1, self.node_data.shape[1]):
+      u = Function(V)
+      u.vector()[vertex_to_dof_map(V)] = self.node_data[:, i].copy()
+      components.append(u)
+     
+    VV = VectorFunctionSpace(mesh, "CG", 1, self.node_data.shape[1]-1)
+    res = Function(VV, name=name)
+    assign(res, components)
+    return res
+
+converter = INPConverter()
+converter.read_inp("multiple_materials.inp")
+File("mesh.pvd") << converter.mesh()
+File("data.pvd") << converter.data()
+File("domains.pvd") << converter.domains()
