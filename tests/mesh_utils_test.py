3628c95af4bb58f8651dc5b102de3c8066f5a598
Florian Bruckner <e0425375@gmail.com>
2015-11-12 22:44:12 +0100

    use different meshes for merging test case

--- tests/mesh_utils_test.py
+++ tests/mesh_utils_test.py
@@ -19,7 +19,7 @@ class MeshUtilsTest(unittest.TestCase):
   def test_add_domains(self):
     mesh1 = Mesh("mesh/mesh_with_domains.xml.gz")
     mesh1b = Mesh("mesh/mesh_with_domains.xml.gz")
-    mesh1b.translate(Point(-1.1,1.1,0.0))
+    mesh1b.translate(Point(-2.1,1.1,0.0))
     mesh2 = UnitCubeMesh(1,1,1)
     mesh2.translate(Point(1.1,0.0,0.0))
     mesh3 = UnitCubeMesh(1,2,2)
@@ -38,10 +38,10 @@ class MeshUtilsTest(unittest.TestCase):
       mt.add(mesh3) # add domain without ID -> error
 
     with self.assertRaises(ValueError):
-      mt.add(mesh1, [5,6,7]) # use domain_id map of wrong type -> error
+      mt.add(mesh1b, [5,6,7]) # use domain_id map of wrong type -> error
     with self.assertRaises(ValueError):
-      mt.add(mesh1, {1:5,2:6,4:7}) # use domain_id map with missing domain -> error
-    mt.add(mesh1, {1:5,2:6,3:7,4:8}) # use domain_id map (with additional domain)
+      mt.add(mesh1b, {1:5,2:6,4:7}) # use domain_id map with missing domain -> error
+    mt.add(mesh1b, {1:5,2:6,3:7,4:8}) # use domain_id map (with additional domain)
 
     domains = MeshFunction("size_t", mt.mesh(), 3, mt.mesh().domains())
     self.assertEqual(mt.mesh().num_cells(), 12006)
@@ -55,7 +55,7 @@ class MeshUtilsTest(unittest.TestCase):
 
     mt.add(mesh3) # add mesh without id
     with self.assertRaises(ValueError): # add mesh with domain IDs -> error
-      mt.add(mesh1)
+      mt.add(mesh1b)
 
     self.assertEqual(mt.mesh().num_cells(), 30)
     self.assertEqual(mt.mesh().num_vertices(), 26)
