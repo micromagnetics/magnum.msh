1532a1edb1591206655f643a6b3e534aa50a620a
Florian Bruckner <e0425375@gmail.com>
2015-11-12 12:10:06 +0100

    add mesh utils

---
+++ tests/mesh_utils_test.py
@@ -0,0 +1,20 @@
+import unittest
+from dolfin import *
+from magnummsh import *
+
+class MeshUtilsTest(unittest.TestCase):
+  def test_add(self):
+    mt = MeshTool()
+
+    mesh1 = UnitCubeMesh(1,2,2)
+    mesh2 = UnitCubeMesh(1,1,1)
+    mesh2.translate(Point(1.1,0.0,0.0))
+
+    mt.add(mesh1)
+    mt.add(mesh2)
+
+    self.assertEqual(mt.mesh().num_cells(), 30)
+    self.assertEqual(mt.mesh().num_vertices(), 26)
+
+if __name__ == '__main__':
+    unittest.main()
